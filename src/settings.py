98fcf006c470a7ccc8f862cac64bc0d589f59dc2
Mori Keli <kelimbth@gmail.com>
2022-11-18 13:12:18 +0300

    added a message tag

--- src/settings.py
+++ src/settings.py
@@ -118,11 +118,13 @@ STATICFILES_DIRS = [
 MEDIA_URL = 'mediaFiles/'
 MEDIA_ROOT = os.path.join(BASE_DIR/'mediaFiles/')
 
-from django.contrib.messages import constants as messages
-MESSAGE_TAGS = {
-    messages.ERROR: 'danger',
-}
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
