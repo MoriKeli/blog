295a198218e72c06b96693c87b913d18b7878fcc
Mori Keli <kelimbth@gmail.com>
2022-11-22 12:43:15 +0300

    added a function to generate id

--- users/signals.py
+++ users/signals.py
@@ -1,7 +1,7 @@
 from django.db.models.signals import pre_save, post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
-from .models import Profile, Posts
+from .models import Profile, Posts, Comments
 import uuid
 
 
@@ -36,9 +36,15 @@ def generate_BlogId(sender, instance, **kwargs):
     if instance.id == "":
         instance.id = str(uuid.uuid4()).replace('-', '')[:12]
 
+@receiver(pre_save, sender=Comments)
+def generate_commentsId(sender, instance, **kwargs):
+    if instance.id == "":
+        instance.id = str(uuid.uuid4()).replace('-', '')[:12]
+
 
 @receiver(post_save, sender=User)
 def save_userprofile(sender, instance, created, **kwargs):
     if created:
         if instance.is_staff is False and instance.is_superuser is False:
             Profile.objects.create(name=instance)
+
