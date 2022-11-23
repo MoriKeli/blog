d770bb2d7b1249759fbeb57011c3fc5619572592
Mori Keli <kelimbth@gmail.com>
2022-11-23 12:19:23 +0300

    styled admin panel

--- users/admin.py
+++ users/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Profile, Posts
 
 admin.site.register(Profile)
-admin.site.register(Posts)
+@admin.register(Posts)
+class PostsTable(admin.ModelAdmin):
+    list_display = ['blogger', 'title', 'posted']
+    search_fields = ['title', 'blogger']
+    
