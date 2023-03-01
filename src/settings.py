85a3196b5f7c068c1947fa0c2a27f6285df1b8f8
brocode <kelimbth@gmail.com>
2023-03-01 17:32:00 +0300

    preparing to deploy project

--- src/settings.py
+++ src/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y3tj0*h&%@!$r&53v1ah73k^pt7r+q!&^%cv2g(q7ifxu2i$#j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['brocode254.pythonanywhere.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
 
 # Application definition
@@ -110,11 +110,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
 
-]
-STATIC_ROOT = ''
+# ]
+STATIC_ROOT = BASE_DIR/'static/'
 
 MEDIA_URL = 'mediaFiles/'
 MEDIA_ROOT = os.path.join(BASE_DIR/'mediaFiles/')
